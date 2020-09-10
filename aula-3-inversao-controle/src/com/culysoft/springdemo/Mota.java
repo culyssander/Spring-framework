b43a93b01af5d00408976b780591b69a3fc44887
Quitumba Culyssander Ferreira <culyssandercentro@gmail.com>
2020-09-10 21:56:49 +0100

    Aula 3 - Inversão de controle IoC

--- aula-3-inversao-controle/src/com/culysoft/springdemo/Mota.java
+++ aula-3-inversao-controle/src/com/culysoft/springdemo/Mota.java
@@ -4,7 +4,7 @@ public class Mota implements Veiculo {
 
 	@Override
 	public void dirigir() {
-		System.out.println("Estou a dirigir a mota...");
+		System.out.println("Estou a dirigir a mota");
 
 	}
 
